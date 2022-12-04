101bf9d7d4e5b53db1f3610c34b45cba67b0eb41
ablertcedarian <almchu@gmail.com>
2022-12-03 23:13:38 -0600

    Day 4

---
+++ AoC2022/4b.py
@@ -0,0 +1,31 @@
+#AoC 2022 4b
+pn = '4'
+import sys
+import re
+import math
+import hashlib
+import operator
+from functools import total_ordering
+from collections import defaultdict 
+
+LETTERS = [x for x in 'abcdefghijklmnopqrstuvwxyz']
+VOWELS = {'a', 'e', 'i', 'o', 'u'}
+CONSONANTS = set(x for x in LETTERS if x not in VOWELS)
+
+input = []
+
+with open(f'{pn}_Input.txt') as file:
+	for line in file:
+		input.append(line.rstrip().split(","))
+
+count = 0
+for ranges in input:
+    a,b = map(int, ranges[0].split("-"))
+    c,d = map(int, ranges[1].split("-"))
+    r1 = set(range(a,b+1))
+    r2 = set(range(c,d+1))
+    count += any(r1 & r2)
+    
+
+print(count)
+
