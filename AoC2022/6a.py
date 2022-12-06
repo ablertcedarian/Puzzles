430c26cdfd8136c2c63a2dc96843034761a7f7fd
ablertcedarian <almchu@gmail.com>
2022-12-05 23:07:55 -0600

    day 6

---
+++ AoC2022/6a.py
@@ -0,0 +1,34 @@
+#AoC 2022 6a
+pn = '6'
+import sys
+import re
+import math
+import hashlib
+import operator
+from functools import total_ordering
+from collections import defaultdict 
+
+LETTERS = [x for x in 'abcdefghijklmnopqrstuvwxyz']
+VOWELS = {'a', 'e', 'i', 'o', 'u'}
+CONSONANTS = set(x for x in LETTERS if x not in VOWELS)
+
+input = []
+
+with open(f'{pn}_Input.txt') as file:
+	for line in file:
+		input.append(line.rstrip())
+print(line)
+
+marker = 0 
+buffer = []
+for i,n in enumerate(line):
+    print(buffer) 
+    if len(set(buffer)) == len(buffer) and len(buffer) == 4:
+        marker = i
+        break
+    else:
+        if len(buffer) > 3:
+            buffer.pop(0)
+        buffer.append(n)
+
+print(marker) 
