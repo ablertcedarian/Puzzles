043591984fdfcd1ecc46f693388f54d2003202f9
ablertcedarian <almchu@gmail.com>
2022-12-06 23:27:57 -0600

    day7, fun parsing--rank 380 on the first part!

---
+++ AoC2022/7b.py
@@ -0,0 +1,75 @@
+#AoC 2022 7b
+pn = '7'
+import sys
+import re
+import math
+import hashlib
+import operator
+from functools import total_ordering
+from collections import defaultdict 
+
+LETTERS = [x for x in 'abcdefghijklmnopqrstuvwxyz']
+VOWELS = {'a', 'e', 'i', 'o', 'u'}
+CONSONANTS = set(x for x in LETTERS if x not in VOWELS)
+
+input = []
+
+with open(f'{pn}_Input.txt') as file:
+	for line in file:
+		input.append(line.rstrip())
+print(input)
+
+tree = defaultdict(defaultdict)
+path = [] 
+for line in input:
+    print(f"line {line}")
+    if line[0] == '$':
+        if line[2:4] == 'cd':
+            if line[5] == '/':
+                curr = tree 
+            elif line[5:7] == '..':
+                path.pop(-1)
+                curr = tree 
+                for subdirec in path:
+                    curr = curr[subdirec]
+            else:
+                direcName = line[5:]
+                curr = curr[direcName]
+                path.append(direcName)
+        elif line[2:4] == 'ls':
+            continue 
+    else:
+        if line[:3] == "dir":
+            direcName = line[4:]
+            curr[direcName] = {}
+        else:
+            size, name = line.split(" ")
+            print(size,name)
+            curr[name] = int(size) 
+
+
+def getSize(sizemin, tree):
+    total = 0 
+    for key in tree.keys():
+        if type(tree[key]) == int:
+            total += tree[key]
+        else:
+            sizemin, temp = getSize(sizemin, tree[key])
+            total += temp 
+    sizemin[f'{total}'] = total 
+    return sizemin, total 
+
+print(tree)
+sizemin, totalSize = getSize({}, tree)
+print(sizemin, totalSize)
+
+current = 70000000 - totalSize
+needed = 30000000 - current
+smallest = totalSize 
+for key in sizemin.keys():
+    print(sizemin[key] - needed)
+    if sizemin[key] >= needed:
+        smallest = min(smallest, sizemin[key])
+        print(f"updating {smallest}")
+print(needed, totalSize)
+print(smallest)
